b81f938288e0e8cf6c5a237eb151f3af24634bc2
JamilaAlsadik1 <w0759725@myscc.ca>
2021-09-23 09:39:10 -0400

    Trying to bring the data for the link prospective

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 // WEB303 Assignment 2
-// Your Name Here
+// Jamila Alsadik
 
+$("document").ready(function(){
+    $("#vprospect").click(function(){
+        var xhttp= new XMLHttpRequest();
+        xhttp.onload=function(){
+            $("div#solution").Html(this.responseText);
+        }
+        xhttp.open("GET","prospect.html");
+        xhttp.send();
+
+    });
+})
