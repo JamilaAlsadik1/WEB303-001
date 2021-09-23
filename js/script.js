a93da1b487047432ffe89985c8cd000f2044f975
JamilaAlsadik1 <w0759725@myscc.ca>
2021-09-23 14:11:05 -0400

    assignment2

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// WEB303 Assignment 2
+// Jamila Alsadik
+
+$("document").ready(function(){
+    $("a#vprospect").click(function(){
+        $("#solution").hide();
+        console.log("is click working?");
+        $("#solution").load("prospect.html").slideDown("slow");
+
+    });
+    $("a#vconvert").click(function(){
+        $("#solution").hide();
+        console.log("is click working?");
+        $("#solution").load("convert.html").slideDown("slow");
+
+    });
+    $("a#vretain").click(function(){
+        $("#solution").hide();
+        console.log("is click working?");
+        $("#solution").load("retain.html").slideDown("slow");
+    });
+});
