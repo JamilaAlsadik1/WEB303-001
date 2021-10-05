a5cdb0688392ce8bd4cb033b03002fe81b3aea5e
JamilaAlsadik1 <w0759725@myscc.ca>
2021-10-05 12:14:37 -0400

    last update

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 $(document).ready(function (){
-    $.getJSON("team.json",function (data){
+ajaxMethod();
+});
+
+ function getJsonMethod() {$.getJSON("team.json",function (data){
+
+	 console.log(data);
 		var teammembers_data='';
         $.each(data.teammembers,function (index,value){
             teammembers_data+='<h3>'+value.name +'</h3>';
 			teammembers_data+= '<h4>'+ value.title +'</h4>';
 			teammembers_data+='<p>' + value.bio + '</p>';
 			$('div#team').append(teammembers_data);
+			teammembers_data='';
         });
-    });
-	$.ajax({
+		
+ });}
+	
+	
+	
+	function ajaxMethod(){$.ajax({
 		url:'team.json',
 		type:'GET',
 		beforeSend:function () {
@@ -18,20 +28,19 @@ $(document).ready(function (){
 			var teammembers_data='';
 
 			  $.each(data.teammembers,function (index,value){
-				  console.log("executing");
             teammembers_data+='<h3>'+value.name +'</h3>';
 			teammembers_data+= '<h4>'+ value.title +'</h4>';
 			teammembers_data+='<p>' + value.bio + '</p>';
 			$('div#team').append(teammembers_data);
+			teammembers_data='';
         });
 		},
-		timeout: 50000,
-		error:`an error occurred`,
+		timeout: 5000,
+		error:function(){
+			alert("an error occurred");
+		},
 		complete:function () {
 			$('h1').css("display","none");
 		}
 
-	});
-
-});
-
+	});}
